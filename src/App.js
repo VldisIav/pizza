15ab2101d3bf91ae4960ce0bc1ddbadf7491a98d
Vlad <thebestemail@mail.ru>
2024-08-13 19:51:02 +0300

    подготовил компоненты, шаблон сайта готов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
+// App() это функциональный компонент
+// функция возвращает html в разметку! отсюда и название
+// то етсь приложение состоит из компонентов
+// но тут не html а jsx! (то есть html + js код запускает at the same time)
+
+
+import React from "react";
+import './/scss/app.scss'
+
+import Header from "./components/Header";
+import Sort from "./components/Sort";
+import Categories from "./components/Categories";
+import PizzaBlock from "./components/PizzaBlock";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <div className="block1">
+        <div className="text">
+          <div className="hello3">test info</div>
+        </div>
+      </div>
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title='Мексиканская' price={500} />
+            <PizzaBlock title='Пепперони' price='250' />
+            <PizzaBlock title='Острая' price='340' />
+            <PizzaBlock title='Мексиканская' price='700' />
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// доку реакта надо читать)
